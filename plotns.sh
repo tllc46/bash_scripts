828dfa0d6de8124d5d1995b9744f238802bd6c12
tllc46 <155712238+tllc46@users.noreply.github.com>
2024-03-16 01:00:13 +0900

    Create plotns.sh

---
+++ plotns.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+grl=4780
+bds=($(cat boundns.gmt))
+file=plotns
+
+meanr=$(echo "0.5*(${bds[0]}+${bds[1]})" | bc)
+bds[2]=$(echo ${bds[2]}+$grl | bc)
+bds[3]=$(echo ${bds[3]}+$grl | bc)
+
+gmt begin $file png
+	gmt set FORMAT_GEO_MAP D
+
+	gmt xyz2grd grid2dvns.z -Ggrid2dvns.grd -I${bds[4]}+n/${bds[5]}+n -R${bds[0]}/${bds[1]}/${bds[2]}/${bds[3]} -ZRB
+	crange=$(gmt grdinfo grid2dvns.grd -T0.05+s)
+	gmt makecpt -Cpolar -I $crange -Z
+	gmt grdimage grid2dvns.grd -JP15c+a+t$meanr+z -BWrSt -Bx0.01 -By5 -By+l"Depth(km)"
+	gmt colorbar -B0.1 -B+l"dVp (km/s)" -DJBC+w8c/0.4c
+gmt end
