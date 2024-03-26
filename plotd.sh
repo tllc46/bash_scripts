f4ca0b1732c8ef52440b07f942a9f44e759f89fb
tllc46 <155712238+tllc46@users.noreply.github.com>
2024-03-26 15:43:27 +0900

    Update plotd.sh

--- plotd.sh
+++ plotd.sh
@@ -4,8 +4,7 @@ bds=($(cat bounddp.gmt))
 file=plotd
 
 gmt begin $file png
-  gmt set FORMAT_GEO_MAP D MAP_FRAME_TYPE plain
-
+	gmt set FORMAT_GEO_MAP D MAP_FRAME_TYPE plain
 	gmt xyz2grd grid2dvd.z -Ggrid2dvd.grd -I${bds[4]}+n/${bds[5]}+n -R${bds[0]}/${bds[1]}/${bds[2]}/${bds[3]} -ZLB
 	crange=$(gmt grdinfo grid2dvd.grd -T0.05+s)
 	gmt makecpt -Cpolar -I $crange -Z
