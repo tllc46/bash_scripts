815096e3dc6475d3c50e5a1f9e8107181f592df0
tllc46 <155712238+tllc46@users.noreply.github.com>
2024-03-27 10:07:00 +0900

    Update plot_events.sh

--- plot_events.sh
+++ plot_events.sh
@@ -8,8 +8,8 @@ event_file=events
 file=event_plot
 
 gmt begin $file png
-  gmt set FORMAT_GEO_MAP +D
-  gmt coast -JE$lon0/$lat0/$hor/$di -Rg -Gdarkgray
-  cut -d '|' -f 3,4 --output-delimiter=' ' $event_file | gmt plot -Gblue -Sc0.07c -:
-  gmt basemap -JP$di+a -R0/360/0/$hor -Bxa30g30 -Byg30
+	gmt set FORMAT_GEO_MAP +D
+	gmt coast -JE$lon0/$lat0/$hor/$di -Rg -Gdarkgray
+	cut -d '|' -f 3,4 --output-delimiter=' ' $event_file | gmt plot -Gblue -Sc0.07c -:
+	gmt basemap -JP$di+a -R0/360/0/$hor -Bxa30g30 -Byg30
 gmt end
