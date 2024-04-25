013778a4b1c27ef3ce657ce9840db35c58bb5052
tllc46 <155712238+tllc46@users.noreply.github.com>
2024-04-25 11:19:52 +0900

    Update plot_rose.sh

--- plot_rose.sh
+++ plot_rose.sh
@@ -9,6 +9,6 @@ file=event_rose
 gmt begin $file png
 	gmt set FORMAT_GEO_MAP +D
 	radius=$(cut -d '|' -f 3,4 --output-delimiter ' ' $event_file | gmt mapproject -Af$lon0/$lat0 -fg -: | gmt rose -A5 -I -i2 -o4 -png foo)
-	cut -d '|' -f 3,4 --output-delimiter ' ' $event_file | gmt mapproject -Af$lon0/$lat0 -fg -: | gmt rose -A5 -Gblue -JX$di -R0/$radius/0/360 -i2
-	gmt basemap -JP$di+a -R0/360/0/$radius -Bxa30g30 -Byg50
+	cut -d '|' -f 3,4 --output-delimiter ' ' $event_file | gmt mapproject -Af$lon0/$lat0 -fg -: | gmt rose -A5 -Gblue -JXy/$di -R0/$radius/0/360 -i2
+	gmt basemap -JPolar/$di+a -R0/360/0/$radius -Bxa30g30 -Byg50
 gmt end show
