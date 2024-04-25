15e79e926da379da64d05e2cca164f737064033a
tllc46 <155712238+tllc46@users.noreply.github.com>
2024-04-25 11:19:31 +0900

    Update plot_stations.sh

--- plot_stations.sh
+++ plot_stations.sh
@@ -7,9 +7,9 @@ gmt begin $file png
 	gmt set FORMAT_GEO_MAP D
 	range=($(sed -En '2,$p' $station_file | cut -d ',' -f 3-4 | gmt info -C -I0.1))
 	range[1]=$(echo ${range[1]}+0.2 | bc)
-	sed -En '2,$p' $station_file | cut -d ',' -f 3-4 | gmt plot -R${range[0]}/${range[1]}/${range[2]}/${range[3]} -Jm10c -B0.2 -Gblue -St0.2c
+	sed -En '2,$p' $station_file | cut -d ',' -f 3-4 | gmt plot -R${range[0]}/${range[1]}/${range[2]}/${range[3]} -Jmerc/10c -B0.2 -Gblue -St0.2c
 	gmt coast -W
-	gmt inset begin -DjBR+o0.1c/0.1c -F+p0.5p -Jm0.5c -R125/130/34/39
+	gmt inset begin -DjBR+o0.1c/0.1c -F+p0.5p -Jmerc/0.5c -R125/130/34/39
 		gmt coast -N1 -W
 		gmt plot -L -Wblue <<EOF
 ${range[1]} ${range[2]}
