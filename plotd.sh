e81fbe878c688f961030f4f16bb7ffaaee2d6448
tllc46 <155712238+tllc46@users.noreply.github.com>
2024-04-25 11:19:12 +0900

    Update plotd.sh

--- plotd.sh
+++ plotd.sh
@@ -8,6 +8,6 @@ gmt begin $file png
 	gmt xyz2grd grid2dvd.z -Ggrid2dvd.grd -I${bds[4]}+n/${bds[5]}+n -R${bds[0]}/${bds[1]}/${bds[2]}/${bds[3]} -ZLB
 	crange=$(gmt grdinfo grid2dvd.grd -T0.05+s)
 	gmt makecpt -Cpolar -I $crange -Z
-	gmt grdimage grid2dvd.grd -Jx200c -Bx0.02 -By0.01
+	gmt grdimage grid2dvd.grd -Jxy/200c -Bx0.02 -By0.01
 	gmt colorbar -B0.1 -B+l"dVp (km/s)" -DJBC+w8c/0.4c
 gmt end
