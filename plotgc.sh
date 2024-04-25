36f7221cff595909f3976857cb883d83915889e4
tllc46 <155712238+tllc46@users.noreply.github.com>
2024-04-25 11:15:01 +0900

    Update plotgc.sh

--- plotgc.sh
+++ plotgc.sh
@@ -8,6 +8,6 @@ gmt begin $file png
 	gmt xyz2grd grid2dvgc.z -Ggrid2dvgc.grd -I${bds[4]}+n/${bds[5]}+n -R${bds[0]}/${bds[1]}/${bds[2]}/${bds[3]} -ZLB
 	crange=$(gmt grdinfo grid2dvgc.grd -T0.05+s)
 	gmt makecpt -Cpolar -I $crange -Z
-	gmt grdimage grid2dvgc.grd -Jxy4c/0.5c -Bx1 -By5
+	gmt grdimage grid2dvgc.grd -Jxy/4c/0.5c -Bx1 -By5
 	gmt colorbar -B0.1 -B+l"dVp (km/s)" -DJBC+w8c/0.4c
 gmt end
