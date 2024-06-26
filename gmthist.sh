80074ee365df95b8a974009e6ddc1d74f443f076
tllc46 <155712238+tllc46@users.noreply.github.com>
2024-06-26 10:47:53 +0900

    Update gmthist.sh

--- gmthist.sh
+++ gmthist.sh
@@ -7,5 +7,5 @@ gmt begin $file png
 	brange=$(gmt info histo.dat -T0.02)
 	xrange=($(gmt info histo.dat -C -I0.02))
 	ymax=$(gmt histogram histo.dat $brange -F -I -o3)
-	gmt histogram histo.dat -JXy/10c/16c $brange -Bx0.1 -By10 -F -Gblue -R${xrange[0]}/${xrange[1]}/0/$ymax
+	gmt histogram histo.dat $brange -Bx0.1 -By10 -F -Gblue -JXy/10c/16c -R${xrange[0]}/${xrange[1]}/0/$ymax
 gmt end
