ba057d2e99c4290027860408a6a029f0b56f84de
tllc46 <155712238+tllc46@users.noreply.github.com>
2024-08-13 17:06:06 +0900

    Update preprocess.sh

--- preprocess.sh
+++ preprocess.sh
@@ -5,7 +5,7 @@ get_stations()
 	channel=SHZ,BHZ
 	starttime=2020-01-01
  	station_file=station.txt
-	wget "https://service.iris.edu/fdsnws/station/1/query?net=$network&sta=$station&cha=$channel&start=$starttime&level=channel&format=text" -O
+	wget "https://service.iris.edu/fdsnws/station/1/query?net=$network&sta=$station&cha=$channel&start=$starttime&level=channel&format=text" -O $station_file
 
 	#station_file, w/ header
 	#knetwk|kstnm|khole|kcmpnm|stla|stlo|stel|stdp|cmpaz|SEEDdip|kinst|scale|Scalefreq|Scaleunits|Samplerate|Start|End
@@ -30,36 +30,50 @@ get_events()
 	minradius=25
 	minmag=5
 	maxmag=10
-	event_file=events
+	event_file=events.txt
 
-	#event_file
+	#event_file, w/o header
 	#EventID | o | evla | evlo | evdp | Author | Catalog | Contributor,ContributorID | imagtyp,mag,MagAuthor | kevnm
-	FetchEvent -s $starttime -e $endtime --radius $lat:$lon:$maxradius:$minradius --mag $minmag:$maxmag | gawk '!a[$2]++' FS='|' >$event_file #duplicate event 제거
+	FetchEvent -s $starttime -e $endtime --radius $lat:$lon:$maxradius:$minradius --mag $minmag:$maxmag | gawk '!a[$2]++' FS='|' >$event_file #중복 event 제거
 }
 
 get_data()
 {
-	starttime=2021-01-01
-	for((i=0;i<365;i++))
+	station_file=station.txt
+ 	avail_post_file=avail_post.txt
+ 	avail_file=avail.txt
+  	data_post_file=data_post.txt
+ 	starttime=2021-01-01
+
+ 	for((i=0;i<365;i++))
 	do
 		echo ------------------ progress: downloading $starttime ------------------ >&2
 		endtime=${starttime}T23:59:59.975
 		year=${starttime:0:4}
         	julday=$(date -d $starttime +%j)
-		gawk 'FNR!=1{if($3=="") print $1,$2,"--",$4,"'$starttime'","'$endtime'"; else print $1,$2,$3,$4,"'$starttime'","'$endtime'"}' FS='|' station.txt >post_avail
-		if wget "https://service.iris.edu/fdsnws/availability/1/extent" --post-file=post_avail -O avail.txt
+		gawk 'FNR!=1{if($3=="") print $1,$2,"--",$4,"'$starttime'","'$endtime'"; else print $1,$2,$3,$4,"'$starttime'","'$endtime'"}' FS='|' $station_file >$avail_post_file
+		if wget "https://service.iris.edu/fdsnws/availability/1/extent" --post-file=$avail_post_file -O $avail_file
 		then
-			gawk 'FNR!=1&&$10==1{sub("Z","",$7); sub("Z","",$8); print $1,$2,$3,$4,$7,$8}' avail.txt >post_data
-			if grep -q 'AU AS' post_data && grep -q 'AU INKA' post_data
+			gawk 'FNR!=1&&$10==1{sub("Z","",$7); sub("Z","",$8); print $1,$2,$3,$4,$7,$8}' $avail_file >$data_post_file
+			if grep -q 'AU AS' $data_post_file && grep -q 'AU INKA' $data_post_file
 			then
 				if [ ! -d /media/tllc46/data01/ANT/rawdata/$year.$julday ]
 				then mkdir /media/tllc46/data01/ANT/rawdata/$year.$julday
 				fi
-				wget "https://service.iris.edu/fdsnws/dataselect/1/query" --post-file=post_data -O /media/tllc46/data01/ANT/rawdata/$year.$julday/AU.miniseed
+				wget "https://service.iris.edu/fdsnws/dataselect/1/query" --post-file=$data_post_file -O /media/tllc46/data01/ANT/rawdata/$year.$julday/AU.miniseed
 			else echo ------------------ progress: no data in $starttime ------------------ >&2
 			fi
 		else echo ------------------ progress: no data in $starttime ------------------ >&2
 		fi
 		starttime=$(date -d "$starttime day" +%Y-%m-%d)
 	done
+
+ 	#avail_post_file, w/o header
+  	#knetwk kstnm khole kcmpnm b e
+  
+  	#avail_file, w/ header
+	#knetwk kstnm khole kcmpnm iqual SampleRate Earliest Latest Updated TimeSpans Restriction
+
+ 	#data_post_file, w/o header
+  	#knetwk kstnm khole kcmpnm b e
 }
